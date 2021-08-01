2de650b1eaf39574ea38e6d9fd8f2528e8f3473c
JAESEONG LEE <lee01042000@gmail.com>
2021-08-01 22:27:56 +0900

    add Serial related code

---
+++ WearingHelmetApp/app/src/main/java/com/example/wearinghelmetapp/BluetoothConnect/Serial/SerialListener.java
@@ -0,0 +1,9 @@
+package com.example.wearinghelmetapp.BluetoothConnect.Serial;
+
+interface SerialListener {
+    void onSerialConnect      ();
+    void onSerialConnectError (Exception e);
+    void onSerialRead         (byte[] data);
+    void onSerialIoError      (Exception e);
+}
+
