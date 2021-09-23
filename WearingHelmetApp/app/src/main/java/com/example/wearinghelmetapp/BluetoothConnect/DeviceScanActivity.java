3638f207b278733dafb0b6a75699aae48e56220b
BEOKS <lee01042000@gmail.com>
2021-09-23 16:37:27 +0900

    Fix(.DeviceScanAvtivity) : 킥보드 탑승 버튼 Visible 미적용 버그 수

--- WearingHelmetApp/app/src/main/java/com/example/wearinghelmetapp/BluetoothConnect/DeviceScanActivity.java
+++ WearingHelmetApp/app/src/main/java/com/example/wearinghelmetapp/BluetoothConnect/DeviceScanActivity.java
@@ -116,58 +116,17 @@ public class DeviceScanActivity extends AppCompatActivity {
                 .setReportDelay(0)
                 .build();
 
-        //filters = new ArrayList<ScanFilter>();
-        //ScanFilter filter = new ScanFilter.Builder().setDeviceName("device명");
 
         //스캔 시작
         scanLeDevice(true);
     }
-
-   /* @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        if (!mScanning) {
-            menu.findItem(R.id.menu_stop).setVisible(false);
-            menu.findItem(R.id.menu_scan).setVisible(true);
-            menu.findItem(R.id.menu_refresh).setActionView(null);
-        } else {
-            menu.findItem(R.id.menu_stop).setVisible(true);
-            menu.findItem(R.id.menu_scan).setVisible(false);
-            menu.findItem(R.id.menu_refresh).setActionView(
-                    R.layout.actionbar_indeterminate_progress);
-        }
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_scan:
-                mLeDeviceListAdapter.clear();
-                scanLeDevice(true);
-                break;
-            case R.id.menu_stop:
-                scanLeDevice(false);
-                break;
-        }
-        return true;
-    }
-*/
     @Override
     protected void onResume() {
         super.onResume();
-
-        // Ensures Bluetooth is enabled on the device.  If Bluetooth is not currently enabled,
-        // fire an intent to display a dialog asking the user to grant permission to enable it.
         if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
-
-        // Initializes list view adapter.
-        //mLeDeviceListAdapter = new LeDeviceListAdapter();
-       // setListAdapter(mLeDeviceListAdapter);
-        //scanLeDevice(true);
     }
 
     @Override
@@ -183,24 +142,8 @@ public class DeviceScanActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        //scanLeDevice(false);
-        //mLeDeviceListAdapter.clear();
     }
-/*
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        final BluetoothDevice device = mLeDeviceListAdapter.getDevice(position);
-        if (device == null) return;
-        final Intent intent = new Intent(this, DeviceControlActivity.class);
-        intent.putExtra(DeviceControlActivity.EXTRAS_DEVICE_NAME, device.getName());
-        intent.putExtra(DeviceControlActivity.EXTRAS_DEVICE_ADDRESS, device.getAddress());
-        if (mScanning) {
-            mBluetoothAdapter.stopLeScan(mLeScanCallback);
-            mScanning = false;
-        }
-        startActivity(intent);
-    }
-*/
+
 
     //스캔
     private void scanLeDevice(final boolean enable) {
@@ -225,75 +168,6 @@ public class DeviceScanActivity extends AppCompatActivity {
         }
         invalidateOptionsMenu();
     }
-
-    /*
-    // Adapter for holding devices found through scanning.
-    private class LeDeviceListAdapter extends BaseAdapter {
-        private ArrayList<BluetoothDevice> mLeDevices;
-        private LayoutInflater mInflator;
-
-        public LeDeviceListAdapter() {
-            super();
-            mLeDevices = new ArrayList<BluetoothDevice>();
-            mInflator = DeviceScanActivity.this.getLayoutInflater();
-        }
-
-        public void addDevice(BluetoothDevice device) {
-            if(!mLeDevices.contains(device)) {
-                mLeDevices.add(device);
-            }
-        }
-
-        public BluetoothDevice getDevice(int position) {
-            return mLeDevices.get(position);
-        }
-
-        public void clear() {
-            mLeDevices.clear();
-        }
-
-        @Override
-        public int getCount() {
-            return mLeDevices.size();
-        }
-
-        @Override
-        public Object getItem(int i) {
-            return mLeDevices.get(i);
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            ViewHolder viewHolder;
-            // General ListView optimization code.
-            if (view == null) {
-                view = mInflator.inflate(R.layout.listitem_device, null);
-                viewHolder = new ViewHolder();
-                viewHolder.deviceAddress = (TextView) view.findViewById(R.id.device_address);
-                viewHolder.deviceName = (TextView) view.findViewById(R.id.device_name);
-                view.setTag(viewHolder);
-            } else {
-                viewHolder = (ViewHolder) view.getTag();
-            }
-
-            BluetoothDevice device = mLeDevices.get(i);
-            final String deviceName = device.getName();
-            if (deviceName != null && deviceName.length() > 0)
-                viewHolder.deviceName.setText(deviceName);
-            else
-                viewHolder.deviceName.setText(R.string.unknown_device);
-            viewHolder.deviceAddress.setText(device.getAddress());
-
-            return view;
-        }
-    }
-*/
-
     // Device scan callback.
     private ScanCallback mLeScanCallback =
             new ScanCallback() {
@@ -459,8 +333,20 @@ public class DeviceScanActivity extends AppCompatActivity {
                     }
                 }
             });
-            ((MaterialButton)findViewById(R.id.kickboard_button)).setVisibility(View.VISIBLE);
-            ((MaterialButton)findViewById(R.id.kickboard_button)).invalidate();
+            new Thread(
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    ((MaterialButton)findViewById(R.id.kickboard_button)).setVisibility(View.VISIBLE);
+                                    ((MaterialButton)findViewById(R.id.kickboard_button)).invalidate();
+                                }
+                            });
+                        }
+                    }
+            ).start();
         }
         @Override
         public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
