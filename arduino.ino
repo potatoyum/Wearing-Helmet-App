4979b2b897137b166e9c265c666dd2c15eeb1d73
JAESEONG LEE <lee01042000@gmail.com>
2021-09-30 13:14:50 +0900

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -1,63 +1,97 @@
+#define BT_RX 10 //블루투스
+#define BT_TX 11 //블루투스
+#define led 13 //led
+#define TRIG 2
+#define ECHO 3
+#define Relay A1
+#define sol A2 //솔레노이드
 #include <SoftwareSerial.h>
-#define BT_RX 3 //블루투스
-#define BT_TX 2 //블루투스
-#define SWW 8 //버튼
-#define led 9 //led
-#include <Servo.h> 
- 
-int servoPin = 6; //서보모터
 
-Servo servo; 
-
-int angle = 0;
-int temp=0;
+int isOpen, h_running;
 SoftwareSerial HM10(BT_RX, BT_TX);
 
 void setup() {
+  pinMode(Relay, OUTPUT);
+  digitalWrite(Relay, HIGH);
+  
   Serial.begin(9600);
   HM10.begin(9600);
-  servo.attach(servoPin);
-  pinMode(led, OUTPUT); 
-  pinMode(SWW, INPUT);
-  temp=0;
-  Serial.write(HM10.available());
 
+  pinMode(led, OUTPUT);
+  
+  pinMode(sol, OUTPUT);
+  
+  digitalWrite(led, LOW);
+  digitalWrite(sol, LOW);
+
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+  isOpen = 0;
+  h_running = 0;
+}
+
+int isHelmetIn(){
+  long duration, distance;
+  
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+
+  duration = pulseIn (ECHO, HIGH);
+  distance = duration * 17 / 1000; 
+
+  //Serial.println(distance);
+
+  return (distance >= 7)? 0 : 1;
+}
+
+void lockOn(){
+  digitalWrite(sol, LOW);
+  isOpen = 0;
+}
+
+void lockOff(){
+  digitalWrite(sol, HIGH);
+  isOpen = 1;
+}
+
+void UVPhase(){
+  //소독 UV-LED 켜기
+  delay(2000);
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(500);
+  digitalWrite(Relay, LOW);
 }
 
 void loop() {
-  int btn = digitalRead(SWW);
-
-  if(HM10.available()){
+  if (HM10.available()) {
     char h = HM10.read();
-    Serial.write(h);
-    if(h=='^'){
-      Serial.write("received");
-      HM10.write("received");
-      servo.write(0); //신호 받으면 열기
-      temp=0;
+
+    if (h == '^') {
+      //HM10.write("get\n");
+      lockOff();
+      h_running++;
     }
-
   }
 
-  if (btn==1 && temp==0){ //잠기는 것
-    Serial.write(btn);
-    servo.write(90);
-    HM10.write("turn");
-
-    //소독 UV-LED 켜기
-    delay(2000);
-    digitalWrite(led, HIGH);
-    delay(1000);
-    digitalWrite(led, LOW);
-    
-  }
+//  Serial.print("running ");
+//  Serial.print(h_running);
+//  Serial.print(" isOpen ");
+//  Serial.print(isOpen);
+//  Serial.print(" helmetIn ");
+//  Serial.println(isHelmetIn());
   
-
-  
-
-/*
-  if(Serial.available()){
-    HM10.write(Serial.read());
+  if(h_running == 1 && isOpen == 1 && isHelmetIn() == 0){
+    lockOn();
   }
-*/
+  if(h_running == 2 && isOpen == 1 && isHelmetIn() == 1){
+    lockOff();
+    h_running = 0;
+    HM10.write("end");
+    UVPhase();
+  }
 }
